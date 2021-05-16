ca99f03c449b6144614e234b216a0aab59eb3aed
Fivezh <Fivezh@gmail.com>
2021-05-16 22:57:54 +0800

    context code

--- golang/context/main.go
+++ golang/context/main.go
@@ -11,8 +11,10 @@ func main() {
 	defer cancel()
 
 	go handle(ctx, 500*time.Millisecond)
+	time.Sleep(time.Millisecond * 600)
+	fmt.Println("hello world") // 这里不阻塞执行
 	select {
-	case <-ctx.Done():
+	case <-ctx.Done(): // 这里阻塞执行，等待ctx超时后，错误保存在ctx.Err()中
 		fmt.Println("main", ctx.Err())
 	}
 }
